24b8e81deec0b385aba4bfa33caaa829e3acf4fe
karthik990 <knag88@gmail.com>
2021-02-16 09:57:15 +0000

    dumpling: dot OS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/dot_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    dot_dumpling-user \
+    dot_dumpling-userdebug \
+    dot_dumpling-eng
